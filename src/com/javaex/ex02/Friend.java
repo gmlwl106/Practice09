2500b4a33a003300a96ade6b7ad7ac4e53dc686c
gmlwl106 <66778894@naver.com>
2022-05-03 16:03:48 +0900

    Practice09. FriendApp(ArrayList)

--- src/com/javaex/ex02/Friend.java
+++ src/com/javaex/ex02/Friend.java
@@ -2,10 +2,49 @@ package com.javaex.ex02;
 
 public class Friend {
 
-    private String name;
-    private String hp;
-    private String school;
-
+	//필드
+	private String name;
+	private String hp;
+	private String school;
+	
+	
+	
+	//생성자
+	public Friend() {
+	}
+	public Friend(String name, String hp, String school) {
+		this.name = name;
+		this.hp = hp;
+		this.school = school;
+	}
+	
+	
+	
+	//메소드 gs
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getHp() {
+		return hp;
+	}
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	
+	
+	//메소드 일반
+	public void showFriends() {
+		System.out.println("이름:"+this.name+" 핸드폰:"+this.hp+" 학교:"+this.school);
+	}
 
 
 }
--- src/com/javaex/ex02/FriendApp.java
+++ src/com/javaex/ex02/FriendApp.java
@@ -1,11 +1,29 @@
 package com.javaex.ex02;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class FriendApp {
 
     public static void main(String[] args) {
 
-
+    	Scanner sc = new Scanner(System.in);
+    	
+    	List<Friend> fList = new ArrayList<Friend>();
+    	
+    	System.out.println("친구를 3명 등록해 주세요 (이름 휴대폰 학교)");
+    	
+    	//친구 등록
+    	for(int i=0; i<3; i++) {
+    		String[] friend = sc.nextLine().split(" "); //문자열 공백으로 나누기
+    		fList.add(new Friend(friend[0], friend[1], friend[2]));
+    	}
+    	
+    	//친구 정보 출력
+    	for(Friend f : fList) {
+    		f.showFriends();
+    	}
     	
     	
     }
